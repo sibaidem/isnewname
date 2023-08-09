8529255582ddf360d44cc3d797042946a9349e75
彭嘉鹏 <2736045645@qq.com>
2023-08-09 09:05:25 +0800

    1

--- src/views/Customers/index.vue
+++ src/views/Customers/index.vue
@@ -1,13 +1,139 @@
 <template>
-    <div>
-        客户分类
-    </div>
+    客户分类
+    <br />
+    <br />
+    <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="120px" class="demo-ruleForm" :size="formSize"
+        status-icon>
+        <el-form-item label="上级分类" prop="region">
+            <el-select v-model="ruleForm.region" placeholder="">
+                <el-option label="Zone one" value="shanghai" />
+                <el-option label="Zone two" value="beijing" />
+            </el-select>
+        </el-form-item>
+        <el-form-item label="分类名称" prop="name">
+            <el-input v-model="ruleForm.name" />
+        </el-form-item>
+        <el-form-item label="所属商户" prop="count">
+            <el-select-v2 v-model="ruleForm.count" placeholder="所属商户" :options="options" />
+        </el-form-item>
+        <el-form-item label="是否启用" prop="delivery">
+            <el-switch v-model="ruleForm.delivery" />
+        </el-form-item>
+
+        <el-form-item>
+            <el-button type="primary" @click="submitForm(ruleFormRef)">
+                保存
+            </el-button>
+            <el-button @click="resetForm(ruleFormRef)">取消</el-button>
+        </el-form-item>
+    </el-form>
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
 
+interface RuleForm {
+    name: string
+    region: string
+    count: string
+    date1: string
+    date2: string
+    delivery: boolean
+    type: string[]
+    resource: string
+    desc: string
+}
+
+const formSize = ref('default')
+const ruleFormRef = ref<FormInstance>()
+const ruleForm = reactive<RuleForm>({
+    name: 'Hello',
+    region: '',
+    count: '',
+    date1: '',
+    date2: '',
+    delivery: false,
+    type: [],
+    resource: '',
+    desc: '',
+})
+
+const rules = reactive<FormRules<RuleForm>>({
+    name: [
+        { required: true, message: 'Please input Activity name', trigger: 'blur' },
+        { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
+    ],
+    region: [
+        {
+            required: true,
+            message: 'Please select Activity zone',
+            trigger: 'change',
+        },
+    ],
+    count: [
+        {
+            required: true,
+            message: 'Please select Activity count',
+            trigger: 'change',
+        },
+    ],
+    date1: [
+        {
+            type: 'date',
+            required: true,
+            message: 'Please pick a date',
+            trigger: 'change',
+        },
+    ],
+    date2: [
+        {
+            type: 'date',
+            required: true,
+            message: 'Please pick a time',
+            trigger: 'change',
+        },
+    ],
+    type: [
+        {
+            type: 'array',
+            required: true,
+            message: 'Please select at least one activity type',
+            trigger: 'change',
+        },
+    ],
+    resource: [
+        {
+            required: true,
+            message: 'Please select activity resource',
+            trigger: 'change',
+        },
+    ],
+    desc: [
+        { required: true, message: 'Please input activity form', trigger: 'blur' },
+    ],
+})
+
+const submitForm = async (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    await formEl.validate((valid, fields) => {
+        if (valid) {
+            console.log('submit!')
+        } else {
+            console.log('error submit!', fields)
+        }
+    })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    formEl.resetFields()
+}
+
+const options = Array.from({ length: 10000 }).map((_, idx) => ({
+    value: `${idx + 1}`,
+    label: `${idx + 1}`,
+}))
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
